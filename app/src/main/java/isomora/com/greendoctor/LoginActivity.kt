b86100fa5ea9ce32ff890dd0fc7f2d811b906df7
atharvapuranik25 <atharva.puranik@avantika.edu.in>
2023-03-17 00:59:08 +0530

    first commit

---
+++ app/src/main/java/isomora/com/greendoctor/LoginActivity.kt
@@ -0,0 +1,112 @@
+package isomora.com.greendoctor
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+
+    lateinit var etEmail: EditText
+    lateinit var etPassword:EditText
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = Firebase.auth
+        viewInitializations()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+//            reload()
+            //goto main activity
+        }
+    }
+
+    fun viewInitializations() {
+
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+
+
+        // To show back button in actionbar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // Checking if the input in form is valid
+    fun validateInput(): Boolean {
+
+        if (etEmail.text.toString().equals("")) {
+            etEmail.setError("Please Enter Email")
+            return false
+        }
+        if (etPassword.text.toString().equals("")) {
+            etPassword.setError("Please Enter Password")
+            return false
+        }
+
+
+        // checking the proper email format
+        if (!isEmailValid(etEmail.text.toString())) {
+            etEmail.setError("Please Enter Valid Email")
+            return false
+        }
+
+        return true
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    // Hook Click Event
+
+    fun performSignUp (view: View) {
+        if (validateInput()) {
+
+            // Input is valid, here send data to your server
+
+
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        //stay on login
+                    }
+                }
+
+            Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show()
+            // Here you can call you API
+
+        }
+    }
+
+}
---
+++ app/src/main/java/isomora/com/greendoctor/SignupActivity.kt
@@ -0,0 +1,138 @@
+package isomora.com.greendoctor
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SignupActivity : AppCompatActivity() {
+
+    lateinit var etFirstName: EditText
+    lateinit var etLastName:EditText
+    lateinit var etEmail: EditText
+    lateinit var etPassword:EditText
+    lateinit var etRepeatPassword:EditText
+    val MIN_PASSWORD_LENGTH = 6;
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+        auth = Firebase.auth
+        viewInitializations()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+//            reload()
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun viewInitializations() {
+        etFirstName = findViewById(R.id.et_first_name)
+        etLastName = findViewById(R.id.et_last_name)
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+        etRepeatPassword = findViewById(R.id.et_repeat_password)
+
+        // To show back button in actionbar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // Checking if the input in form is valid
+    fun validateInput(): Boolean {
+        if (etFirstName.text.toString().equals("")) {
+            etFirstName.setError("Please Enter First Name")
+            return false
+        }
+        if (etLastName.text.toString().equals("")) {
+            etLastName.setError("Please Enter Last Name")
+            return false
+        }
+        if (etEmail.text.toString().equals("")) {
+            etEmail.setError("Please Enter Email")
+            return false
+        }
+        if (etPassword.text.toString().equals("")) {
+            etPassword.setError("Please Enter Password")
+            return false
+        }
+        if (etRepeatPassword.text.toString().equals("")) {
+            etRepeatPassword.setError("Please Enter Repeat Password")
+            return false
+        }
+
+        // checking the proper email format
+        if (!isEmailValid(etEmail.text.toString())) {
+            etEmail.setError("Please Enter Valid Email")
+            return false
+        }
+
+        // checking minimum password Length
+        if (etPassword.text.length < MIN_PASSWORD_LENGTH) {
+            etPassword.setError("Password Length must be more than " + MIN_PASSWORD_LENGTH + "characters")
+            return false
+        }
+
+        // Checking if repeat password is same
+        if (!etPassword.text.toString().equals(etRepeatPassword.text.toString())) {
+            etRepeatPassword.setError("Password does not match")
+            return false
+        }
+        return true
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    // Hook Click Event
+
+    fun performSignUp (view: View) {
+        if (validateInput()) {
+
+            // Input is valid, here send data to your server
+
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            val repeatPassword = etRepeatPassword.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        //goto signup again
+                    }
+                }
+
+            Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show()
+            // Here you can call you API
+
+        }
+    }
+
+}
